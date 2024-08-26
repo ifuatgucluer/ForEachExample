59d503bdda30a10a77a753ecc3ff185abeafd084
Simply Done <guclfuat741@gmail.com>
2024-08-26 09:57:54 +0300

    first commit

---
+++ src/test/java/ForEachExample.java
@@ -0,0 +1,15 @@
+
+
+    public class ForEachExample {
+        public static void main(String[] args) {
+            // Bir dizi oluşturma
+            String[] fruits = {"Üzüm", "Muz", "Erik", "Kayisi "};
+
+            // For-Each döngüsü kullanarak dizideki her bir elemanı yazdırma
+            System.out.println("Meyve Listesi:");
+            for (String fruit : fruits) {
+                System.out.println(fruit);
+            }
+        }
+    }
+
